323c2fa91fcba26006b6600d69773802d337ef4a
Bartosz_Automated_Dev <bartekfc41@gmail.com>
2025-09-29 22:09:04 +0200

    Refactor the code in the scraper.py to be modular
    TODO: add description to be scraped and fix overflown text not getting located.

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -5,84 +5,108 @@ from validator import url_validator
 import json
 from pathlib import Path
 
-class Browser:
-    def __init__(self):
-        """The function initializes a Python class with a base URL for scraping book information."""
+class Book:
+    """Book structure class"""
+    def __init__(self, title, price, upc, product_type, price_without_tax, price_with_tax,
+                tax,
+                availability,
+                number_of_reviews,
+                star_rating,
+                url
+        ):
+        
+        self.title: str = title
+        self.price: str = price
+        self.upc: str = upc
+        self.product_type: str = product_type
+        self.price_without_tax: str = price_without_tax
+        self.price_with_tax: str = price_with_tax
+        self.tax: str = tax
+        self.availability: str = availability
+        self.number_of_reviews: str = number_of_reviews
+        self.star_rating: str = star_rating
+        self.url: str = url
 
-        self.BASE_URL = "https://books.toscrape.com/" #TODO TEST that the page is accessible
+    def to_dict(self) -> dict[str, Any]:
+        return self.__dict__
     
-    def save_to_file(self, file):
+class Scraper:
+    def __init__(self, page: Page, base_url: str):
+        self.page = page
+        self.base_url = base_url
+
+    def get_books_urls(self) -> list[str]:
+        """Get all book URLs from current page."""
+        books = self.page.query_selector_all("article.product_pod h3 a")
+        return [url_validator(urljoin(self.page.url, book_url.get_attribute("href"))) for book_url in books if book_url]
+
+    def scrape_book_data(self, url: str) -> Book:
+        self.page.goto(url, wait_until = "domcontentloaded")
+
+        title: str = self.page.query_selector("div.product_main h1").inner_text()
+        price: str = self.page.query_selector("p.price_color").inner_text()
+        upc: str = self.page.query_selector("table.table-striped tr:first-child td").inner_text()
+        product_type: str = self.page.query_selector("table.table-striped tr:nth-child(2) td").inner_text()
+        price_without_tax: str = self.page.query_selector("table.table-striped tr:nth-child(3) td").inner_text()
+        price_with_tax: str = self.page.query_selector("table.table-striped tr:nth-child(4) td").inner_text()
+        tax: str = self.page.query_selector("table.table-striped tr:nth-child(5) td").inner_text()
+        availability: str = self.page.query_selector("table.table-striped tr:nth-child(6) td").inner_text()
+        number_of_reviews: str = self.page.query_selector("table.table-striped tr:nth-child(7) td").inner_text()
+        star_rating: str = self.page.query_selector("p.star-rating").get_attribute("class").split()[-1]
+
+        return Book(title, price, upc, product_type, price_without_tax, price_with_tax, tax, availability, number_of_reviews, star_rating, url)
+    
+    def get_next_page_url(self):
+        next_page_btn = self.page.query_selector("li.next a")
+        if not next_page_btn:
+            return None
+        
+        return url_validator(urljoin(self.page.url, next_page_btn.get_attribute("href")))
+
+class Browser:
+    def __init__(self, base_url = "https://books.toscrape.com/"):
+
+        self.BASE_URL = base_url
+    
+    def save_to_file(self, books: list[Book], file_name: str) -> None:
         folder: str = "outputs"
-        file_name: str = "book_data.json"
-        file_path: Path = Path(folder, file_name)
+        file_path: str = Path(folder, file_name)
 
         with open(file_path, "w", encoding = "utf-8") as f: # makes book_data.json file in the outputs folder 
-            json.dump(file, f, indent = 2, ensure_ascii = False)
-    
-    def main_scraper(self) -> None:
+            json.dump([book.to_dict() for book in books if book], f, indent = 2, ensure_ascii = False)
 
-        #TODO Test and validate the scraped Data
+    def main(self) -> None:
 
         #! Cookies and cups None description
         #! Add description back, as it was making an error
 
+        books_data: list[Book] = []
         with sync_playwright() as p:
             browser = p.chromium.launch(headless = False)
             page = browser.new_page()
             page.goto(self.BASE_URL, wait_until="domcontentloaded")
             
-            books_data: list[dict[str, Any]] = []
+            scraper = Scraper(page, page.url)
+
             while True:
-
                 current_page_url = page.url
+                try:
 
-                books = page.query_selector_all("article.product_pod h3 a")
-                books_urls = [url_validator(urljoin(page.url, book_url.get_attribute("href"))) if book_url else None for book_url in books]
+                    books_urls = scraper.get_books_urls()
 
-                for book_url in books_urls:
-                    # Visits every book page and scrapes relevant info
-                    page.goto(book_url, wait_until = "domcontentloaded")
+                    for book_url in books_urls: # Visits every book page and scrapes relevant info
+                        book = scraper.scrape_book_data(book_url)
+                        books_data.append(book)
 
-                    title: str = page.query_selector("div.product_main h1").inner_text()
-                    price: str = page.query_selector("p.price_color").inner_text()
-                    upc: str = page.query_selector("table.table-striped tr:first-child td").inner_text()
-                    product_type: str = page.query_selector("table.table-striped tr:nth-child(2) td").inner_text()
-                    price_without_tax: str = page.query_selector("table.table-striped tr:nth-child(3) td").inner_text()
-                    pric_with_tax: str = page.query_selector("table.table-striped tr:nth-child(4) td").inner_text()
-                    tax: str = page.query_selector("table.table-striped tr:nth-child(5) td").inner_text()
-                    availability: str = page.query_selector("table.table-striped tr:nth-child(6) td").inner_text()
-                    number_of_reviews: str = page.query_selector("table.table-striped tr:nth-child(7) td").inner_text()
-                    star_rating: str = page.query_selector("p.star-rating").get_attribute("class").split()[-1]
-                    url = book_url
+                    page.goto(current_page_url, wait_until= "domcontentloaded")
+                    next_page = scraper.get_next_page_url()
+                    page.goto(next_page, wait_until = "domcontentloaded")
 
-                    book_data: dict[str, Any] = {
-                        "title": title,
-                        "price": price,
-                        "upc": upc,
-                        "product_type": product_type,
-                        "price_without_tax": price_without_tax,
-                        "price_with_tax": pric_with_tax,
-                        "tax": tax,
-                        "availability": availability,
-                        "number_of_reviews": number_of_reviews,
-                        "star_rating": star_rating,
-                        "book_url": url
-                    }
-
-                    books_data.append(book_data)
-
-                page.goto(current_page_url, wait_until = "domcontentloaded")
-                next_page_btn = page.query_selector("li.next a")
-
-                if not next_page_btn:
+                except Exception as e:
+                    print(f"error: {e}")
                     break
 
-                next_page_url = next_page_btn.get_attribute("href")
-                full_next_page_url = url_validator(urljoin(current_page_url, next_page_url))
-
-                page.goto(full_next_page_url, wait_until = "domcontentloaded")
-
-            self.save_to_file(books_data)
+            self.save_to_file(books_data, "book_data.json")
 
 if __name__ == "__main__":
-    Browser().main_scraper()
+    Browser().main()
