ffdf96686607f3437f8dc0e37629f3e5bec954a0
Bartosz_Automated_Dev <bartekfc41@gmail.com>
2025-09-29 22:28:18 +0200

    Fix the error after script finishes its work.
    deleted unneeded dependencies.

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,6 +1,6 @@
-from playwright.sync_api import sync_playwright, Page, ElementHandle
+from playwright.sync_api import sync_playwright, Page
 from urllib.parse import urljoin
-from typing import List, Any
+from typing import Any
 from validator import url_validator
 import json
 from pathlib import Path
@@ -41,6 +41,10 @@ class Scraper:
         return [url_validator(urljoin(self.page.url, book_url.get_attribute("href"))) for book_url in books if book_url]
 
     def scrape_book_data(self, url: str) -> Book:
+        '''The code snippet you provided is defining a method within the `Scraper` class called
+        `scrape_book_data`. This method is responsible for scraping data from a specific book page
+        given its URL.'''
+
         self.page.goto(url, wait_until = "domcontentloaded")
 
         title: str = self.page.query_selector("div.product_main h1").inner_text()
@@ -59,7 +63,7 @@ class Scraper:
     def get_next_page_url(self):
         next_page_btn = self.page.query_selector("li.next a")
         if not next_page_btn:
-            return None
+            return False
         
         return url_validator(urljoin(self.page.url, next_page_btn.get_attribute("href")))
 
@@ -70,15 +74,14 @@ class Browser:
     
     def save_to_file(self, books: list[Book], file_name: str) -> None:
         folder: str = "outputs"
-        file_path: str = Path(folder, file_name)
+        file_path: Path = Path(folder, file_name)
 
         with open(file_path, "w", encoding = "utf-8") as f: # makes book_data.json file in the outputs folder 
             json.dump([book.to_dict() for book in books if book], f, indent = 2, ensure_ascii = False)
 
-    def main(self) -> None:
-
-        #! Cookies and cups None description
-        #! Add description back, as it was making an error
+    def run(self) -> None:
+        '''Main method that runs the script.'''
+        #! Add description back, as it was making an error with overflown text
 
         books_data: list[Book] = []
         with sync_playwright() as p:
@@ -109,4 +112,4 @@ class Browser:
             self.save_to_file(books_data, "book_data.json")
 
 if __name__ == "__main__":
-    Browser().main()
+    Browser().run()
