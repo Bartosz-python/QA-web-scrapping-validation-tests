876a2d7e4927ca3041e3b12b55b8c59cdf352aef
Bartosz_Automated_Dev <bartekfc41@gmail.com>
2025-10-04 17:50:57 +0200

    Changed is_existing test

--- tests/confest.py
+++ tests/confest.py
@@ -16,15 +16,15 @@ def is_proper_type_data() -> list:
     (True, bool, True),
 ]
 
-# def browser():
-#     """Works with page function"""
-#     with sync_playwright() as p:
-#         browser = p.chromium.launch(headless = True)
-#         yield browser
-#         browser.close()
+def browser():
+    """Works with page function"""
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless = True)
+        yield browser
+        browser.close()
 
-# def page(browser: browser):
-#     """Works based on browser function"""
-#     page = browser.new_page()
-#     yield page
-#     page.close()
+def page(browser: browser):
+    """Works based on browser function"""
+    page = browser.new_page()
+    yield page
+    page.close()
